4d5d532f1a964e741bf97dab4e74babe7afe50bd
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-01-23 10:02:39 +0900

    Update layout

--- src/components/ReferenceList.vue
+++ src/components/ReferenceList.vue
@@ -34,17 +34,19 @@ const onSubmit = () => {
   <div class="">
     <div class="">
       <edit-reference-form :reference="state.reference" @submit="onSubmit" @cancel="state.isEditMode = false" v-show="state.isEditMode"/>
-      <div v-show="!state.isEditMode" class="panel pure-u-sm-1-3 pure-u-md-1-4" v-for="item in referenceList" :key="item">
-        <div class="panel-header">
-          <span>{{item.name}}</span>
-        </div>
-        <div class="panel-body">
-          <small v-if="item.description">{{item.description}}</small>
-        </div>
-        <div class="panel-footer">
-          <a v-if="item.url" class="button-small pure-button" target="_blank" :href="item.url">表示</a>
-          <button @click="editItem(item)" class="button-small pure-button">編集</button>
-          <button @click="deleteItem(item)" class="button-small pure-button">削除</button>
+      <div class="pure-u-sm-1-3 pure-u-md-1-4">
+        <div v-show="!state.isEditMode" class="panel" v-for="item in referenceList" :key="item">
+          <div class="panel-header">
+            <span>{{item.name}}</span>
+          </div>
+          <div class="panel-body">
+            <small v-if="item.description">{{item.description}}</small>
+          </div>
+          <div class="panel-footer">
+            <a v-if="item.url" class="button-small pure-button" target="_blank" :href="item.url">表示</a>
+            <button @click="editItem(item)" class="button-small pure-button">編集</button>
+            <button @click="deleteItem(item)" class="button-small pure-button">削除</button>
+          </div>
         </div>
       </div>
     </div>
