7d8fc06f52097d05df4113fd78e410ff547bd484
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-02-06 13:54:35 +0900

    Dev: Use environment variables

--- src/http_client.js
+++ src/http_client.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://www.quickrefsserver.top/api',
-    // baseURL: 'https://localhost:7090/api',
-    // baseURL: 'https://localhost:44329/api',
-    // baseURL: 'https://192.168.2.56:7090/api',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 1000,
   });
 
