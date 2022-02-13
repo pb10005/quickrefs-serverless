34d5711d1d74bf92a1cc6488e376e9f00aac82f7
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-02-13 16:23:51 +0900

    Layout: update layout

--- src/components/AddKnowledgeForm.vue
+++ src/components/AddKnowledgeForm.vue
@@ -22,23 +22,27 @@ const sendData = () => {
 };
 </script>
 <template>
-  <div class="bg-tertiary py-3 px-2 h-px">
-    <form @submit.prevent="sendData" class="">
-      <h2 class="text-headline text-center font-bold text-2xl mb-3">ナレッジを新規作成</h2>
-      <div class="mb-2">
-        <input id="name" type="text" placeholder="表示名" class="w-full h-10 px-4 outline-none" v-model="state.name" required>
-      </div>
-      <div>
-        <textarea id="description" placeholder="説明" class="w-full h-60 px-4 outline-none" type="text" v-model="state.description"></textarea>
-      </div>
-      
-      <label class="inline-flex items-center mt-3 mb-3">
-          <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600" v-model="state.isPrivate"><span class="ml-2 text-gray-700">非公開にする</span>
-      </label>
-      <div class="pure-controls">
-        <button class="bg-accent rounded-md font-semibold py-2 w-full">作成</button>
-      </div>
-    </form>
+  <div>
+    <div class="bg-tertiary py-3 px-2 mt-2 rounded-lg">
+      <form @submit.prevent="sendData" class="">
+        <h2 class="text-headline text-center font-bold text-2xl mb-3">ナレッジを新規作成</h2>
+        <div class="mb-2">
+          <p>表示名</p>
+          <input id="name" type="text" placeholder="表示名" class="w-full h-10 px-4 outline-none" v-model="state.name" required>
+        </div>
+        <div>
+          <p>説明</p>
+          <textarea id="description" placeholder="説明" class="w-full h-60 px-4 outline-none" type="text" v-model="state.description"></textarea>
+        </div>
+        
+        <label class="inline-flex items-center mt-3 mb-3">
+            <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600" v-model="state.isPrivate"><span class="ml-2 text-gray-700">非公開にする</span>
+        </label>
+        <div class="pure-controls">
+          <button class="bg-accent rounded-md px-4 py-2">作成</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
